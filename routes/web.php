eb29cbcdd5a5d32f864485f0056633474899f572
bagas <bagaskalihputra.bkp@gmail.com>
2023-05-18 00:05:20 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\comments;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/login', [LoginController::class, 'index']);
+
+
+Route::get('pulau/jawa', function () {
+    return view('jawa');
+});
+
+Route::get('pulau/', function () {
+    return redirect('pulau/jawa');
+});
+
+Route::get('/jogja', function () {
+    return view('jogja');
+});
+
+Route::get('review/jogja', function () {
+    return view('review-jogja', [
+        "komentar" => comments::all()
+    ]);
+});
